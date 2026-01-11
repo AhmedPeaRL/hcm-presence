f1fdc3373b1d41b7bc45531632e1223b729d0a01
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-01-11 02:01:21 +0200

    Create manifest-loader.js

---
+++ manifest-loader.js
@@ -0,0 +1,9 @@
+async function loadManifest(url) {
+  try {
+    const res = await fetch(url, { cache: "no-store" });
+    if (!res.ok) return null;
+    return await res.json();
+  } catch {
+    return null;
+  }
+}
